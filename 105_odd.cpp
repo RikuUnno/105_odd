6a2bd75305b16278237b7ed3c6c81bcc87a25299
RikuUnno <riku.unno@gmail.com>
2024-11-01 12:08:05 +0900

    プロジェクト ファイルを追加します。

---
+++ 105_odd.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int oddN = 0;
+    int divisorsCount = 0;
+    int count = 0;
+
+    for (int j = 1; j <= 200; j += 2)
+    {
+        oddN = j;
+        divisorsCount = 0;
+        for (int i = 1; i <= oddN; i++)
+        {
+            if (oddN % i == 0)
+            {
+                divisorsCount++;
+                cout << oddN << "の約数：" << i << endl;
+            }
+        }
+
+        cout << oddN << "の約数は" << divisorsCount << "個" << endl;
+
+        if (divisorsCount == 8)
+        {
+            count++;
+            cout << "j = " << j << endl;
+        }
+
+        cout << endl;
+
+    }
+
+    cout << "約数8個持つ数の個数＝" << count << endl;
+
+}
